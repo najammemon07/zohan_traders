6ee6245b4aea768d819f4f42338945b2de661629
najammemon07 <aishanajam66@gmail.com>
2026-08-22 02:24:21 +0500

    feat: add React application entry

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+import App from './App';
+import './styles.css';
+
+const system = createSystem(defaultConfig);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+);
